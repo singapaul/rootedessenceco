db511094e95c60fd988c47c1739f78fa4af2e6e4
PaulHardman <paul.hardman@victorianplumbing.co.uk>
2024-05-26 11:03:17 +0100

    wip

--- src/components/organisms/Contact/ContactForm.tsx
+++ src/components/organisms/Contact/ContactForm.tsx
@@ -56,7 +56,7 @@ export const ContactForm = () => {
       values;
 
     try {
-      const res = await fetch("http://localhost:3000/api/email", {
+      const res = await fetch("/api/email", {
         method: "POST",
         body: JSON.stringify({
           businessName,
